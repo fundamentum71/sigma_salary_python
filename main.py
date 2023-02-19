409bb4dc34b9c807ad2c755a6beab84954a7709e
unknown <@b4s4c428kucherov.sigma2.local>
2023-02-19 14:36:20 +0300

    получение денег за выходной день

---
+++ main.py
@@ -0,0 +1,59 @@
+holidays = {
+    # 29: 5,
+    4: 5,
+    5: 5,
+    18: 5,
+    19: 5,
+}
+
+holidaysInput = {}
+
+num_key = []
+arraySun = []
+arraySat = []
+normalRateHour = 0
+higherRateHour = 0
+
+for key in holidays:
+    num_key.append(key)
+
+print(num_key)
+
+for key, item in holidays.items():
+    print('дата: ', key)
+    print("часов: ", item)
+    if num_key.count(key + 1) != 0:
+        print('суббота')
+        print('*' * 50)
+        arraySat.append(item)
+        continue
+
+    if num_key.count(key - 1) != 0:
+        print('воскресенье')
+        print('*' * 50)
+        arraySun.append(item)
+        continue
+
+    print('Одиночный день')
+    print('*' * 50)
+    normalRateHour += item
+
+print('Парные вых. Субботы: ', arraySat)
+print('Парные вых. Воскресение: ', arraySun)
+
+arraySum = map(sum, zip(arraySat, arraySun))
+arraySumList = list(arraySum)
+print(arraySumList)
+
+for el in arraySumList:
+    print(el)
+    if el > 7:
+        normalRateHour += 7
+        higherRateHour += el - 7
+
+print('Нормальная ставка :', normalRateHour, 'Повышенная ставка :', higherRateHour)
+
+
+hourPrice = 287
+overPriceToHolidays = normalRateHour * hourPrice + higherRateHour * 2 * hourPrice
+print('Деньги за выходные: ', overPriceToHolidays, 'руб.')
